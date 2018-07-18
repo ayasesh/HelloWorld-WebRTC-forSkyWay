4e000f272d7976a060d3afb39a8bf6835ce08e2c
otagawa yo <otagawa@fenrir.co.jp>
2018-07-18 17:45:53 +0900

    バグ修正2

--- script-phone.js
+++ script-phone.js
@@ -2,6 +2,8 @@ let peer;
 
 let localStream;
 
+var call;
+
 /* eslint-disable require-jsdoc */
 function initPeer(key) {
     // Peer object
@@ -23,40 +25,10 @@ function initPeer(key) {
     peer.on('error', err => {
         alert(err.message);
     });
-
-    // Click handlers setup
-    $('#broadcast').on('submit', e => {
-        e.preventDefault();
-        navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(stream => {
-            $('#video').get(0).srcObject = stream;
-            localStream = stream;
-        }).catch(err => {
-            $('#step1-error').show();
-            console.error(err);
-        });
-    });
-
-    $('#watch').on('submit', e => {
-        e.preventDefault();
-        // Initiate a call!
-        console.log($('#callto-id').val());
-        const call = peer.call($('#callto-id').val());
-
-        // Wait for stream on the call, then set peer video display
-        call.on('stream', stream => {
-            const el = $('#video').get(0);
-            el.srcObject = stream;
-            el.play();
-        });
-
-        call.on('close', () => {
-            console.log('connection closed');
-        });
-    });
 };
 
-function call(roomId) {
-    const call = peer.call(roomId);
+function callBroadCast(roomId) {
+    call = peer.call(roomId);
 
     // Wait for stream on the call, then set peer video display
     call.on('stream', stream => {
