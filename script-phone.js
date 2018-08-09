1895979bf9838150aaf7d06939ca86e564c6d060
otagawa yo <otagawa@fenrir.co.jp>
2018-08-09 10:51:23 +0900

    Revert "peer 変数のスコープを変更"
    This reverts commit efc332070ec91956826a6e99d7d4b9464863f9c5.

--- script-phone.js
+++ script-phone.js
@@ -1,58 +1,56 @@
-var peer;
-
 /* eslint-disable require-jsdoc */
 function initPeer(key) {
-    // Peer object
-    peer = new Peer({
-        key: key,
-        debug: 3,
+    // Peer object// Peer object
+  const peer = new Peer({
+    key: key,
+    debug: 3,
+  });
+
+  let localStream;
+
+  peer.on('open', () => {
+    $('#my-id').text(peer.id);
+  });
+
+  // Receiving a call
+  peer.on('call', call => {
+    // Answer the call automatically (instead of prompting user) for demo purposes
+    call.answer(localStream);
+  });
+
+  peer.on('error', err => {
+    alert(err.message);
+  });
+
+  // Click handlers setup
+  $('#broadcast').on('submit', e => {
+    e.preventDefault();
+    navigator.mediaDevices.getUserMedia({audio: true, video: true}).then(stream => {
+      $('#video').get(0).srcObject = stream;
+      localStream = stream;
+    }).catch(err => {
+      $('#step1-error').show();
+      console.error(err);
+    });
+  });
+
+  $('#watch').on('submit', e => {
+    e.preventDefault();
+    // Initiate a call!
+    console.log($('#callto-id').val());
+    const call = peer.call($('#callto-id').val());
+
+    // Wait for stream on the call, then set peer video display
+    call.on('stream', stream => {
+      const el = $('#video').get(0);
+      el.srcObject = stream;
+      el.play();
     });
 
-    let localStream;
-
-    peer.on('open', () => {
-        $('#my-id').text(peer.id);
+    call.on('close', () => {
+      console.log('connection closed');
     });
-
-    // Receiving a call
-    peer.on('call', call => {
-        // Answer the call automatically (instead of prompting user) for demo purposes
-        call.answer(localStream);
-    });
-
-    peer.on('error', err => {
-        alert(err.message);
-    });
-
-    // Click handlers setup
-    $('#broadcast').on('submit', e => {
-        e.preventDefault();
-        navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(stream => {
-            $('#video').get(0).srcObject = stream;
-            localStream = stream;
-        }).catch(err => {
-            $('#step1-error').show();
-            console.error(err);
-        });
-    });
-
-    $('#watch').on('submit', e => {
-        e.preventDefault();
-        // Initiate a call!
-        console.log($('#callto-id').val());
-        const call = peer.call($('#callto-id').val());
-
-        // Wait for stream on the call, then set peer video display
-        call.on('stream', stream => {
-            const el = $('#video').get(0);
-            el.srcObject = stream;
-            el.play();
-        });
-
-        call.on('close', () => {
-            console.log('connection closed');
-        });
-    });
+  });
 };
 
 function call(roomId) {
@@ -60,12 +58,12 @@ function call(roomId) {
 
     // Wait for stream on the call, then set peer video display
     call.on('stream', stream => {
-        const el = $('#video').get(0);
-        el.srcObject = stream;
-        el.play();
+      const el = $('#video').get(0);
+      el.srcObject = stream;
+      el.play();
     });
 
     call.on('close', () => {
-        console.log('connection closed');
+      console.log('connection closed');
     });
 }
